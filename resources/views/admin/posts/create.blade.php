4617ca4a783efa5d676362c6a61ee90fb172f70b
vũ long <vulongepu@gmail.com>
2017-07-07 23:08:12 +0700

    create the post form

--- resources/views/admin/posts/create.blade.php
+++ resources/views/admin/posts/create.blade.php
@@ -5,7 +5,38 @@
 
   <h2 class="text-center">
   	
-      create new post
+      <div class="panel panel-default">
+      	<div class="panel-heading">
+      		Create a new post
+      	</div>
+
+      	<div class="panel-body">
+      		<form action="/post/store" method="post">
+      			
+      			{{ csrf_field() }}
+
+      			<div class="form-group">
+      				<label for="title">Title</label>
+      				<input type="text" name="title" id="" class="form-control">
+      			</div>
+      			<div class="form-group">
+      				<label for="featured">Featured</label>
+      				<input type="file" name="featured" id="" class="form-control">
+      			</div>
+      			<div class="form-group">
+      				<label for="content">Content</label>
+      				<textarea name="content" id="content" cols="5" rows="5" class="form-control"></textarea>
+      			</div>
+                <div class="form-group">
+                	<div class="text-center">
+                		<button class="btn btn-success" type="submit">
+                			Store post
+                		</button>
+                	</div>
+                </div>
+      		</form>
+      	</div>
+      </div>
 
   </h2>
 
